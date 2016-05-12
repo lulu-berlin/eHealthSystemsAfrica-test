b10c8ccb8669a389cee5b14300d104eddf30692d
Ya'ar Hever <yaarhever@gmail.com>
2016-05-12 19:38:15 +0200

    implement pattern-and-string (test question 1)

--- README.md
+++ README.md
@@ -4,6 +4,30 @@ This repository contains my answers to the test I've been given by eHealth Syste
 
 # 1. Pattern and String
 
+## Description:
+
+Given a pattern and a string, find if the string follows the same pattern.
+
+Eg: Pattern : [a, b, b, a], String : "cat dog dog cat"
+
+## Solution:
+
+The implementation can be found in
+[src/js/pattern-and-string.js](src/js/pattern-and-string.js). 
+
+I have assumed that the pattern is an array of distinct objects (whatever
+``a`` and ``b`` refer to in the example). 
+
+It first checks that the number of elements in the pattern array is
+identical to the number of space-separated substrings. Then, a dictionary
+is created mapping the pattern objects to the substrings. For each element
+in the pattern, it is checked if the corresponding substring already
+appeared in the dictionary. If it did, then the current substring must
+match the value it has in the dictionary. Otherwise (if the pattern
+element is new), it should be made sure that the current substring wasn't
+mapped earlier to another pattern element. If all tests pass, the pattern
+matches the string.
+
 # 2. Perfect Number
 
 # 3. Scheduling Meetings
---
+++ src/js/pattern-and-string.js
@@ -0,0 +1,46 @@
+exports.checkStringForPattern = function(pattern, string) {
+    'use strict';
+
+    // Just make sure that pattern is an array and string is a string
+    // this can be omitted if we're sure the input is of the correct type.
+    if (!(pattern instanceof Array)) {
+        throw "'pattern' should be an array";
+    }
+    if (typeof string !== 'string') {
+        throw "'string' should be a string";
+    }
+
+    var substrings = string.split(' ');
+
+    // If the pattern doesn't match in size the number of space-separated
+    // substrings, then it cannot match the string.
+    if (pattern.length !== substrings.length) {
+        return false;
+    }
+
+    var dict = {};
+
+    return pattern.every(function(key, index) {
+        var curSubstring = substrings[index];
+
+        if (key in dict) {
+            // If the pattern-key was already seen, but matching a different substring
+            // it means the pattern doesn't match the string.
+            if (curSubstring !== dict[key]) {
+                return false;
+            }
+        } else {
+            // If the pattern-key is new, but the substring was already assigned to another
+            // pattern-key, it means that the pattern doesn't match the string.
+            if (Object.keys(dict).some(function(key) {
+                return dict[key] === curSubstring;
+            })) {
+                return false;
+            }
+            // Add the pattern-key to the dictionary
+            dict[key] = curSubstring;
+        }
+        // All tests passed, so for this iteration, the pattern matches.
+        return true;
+    });
+};
