b75af6e9cd9b2cf21b28a49ead98c02e2b85ce6d
Ya'ar Hever <yaarhever@gmail.com>
2016-05-17 01:36:55 +0200

    improve tests

--- spec/test1-spec.js
+++ spec/test1-spec.js
@@ -3,41 +3,48 @@
 var pattern_and_string = require('../src/js/pattern-and-string');
 
 describe('checkStringForPattern', function() {
+    var checkStringForPattern = pattern_and_string.checkStringForPattern;
+
     it("should return false if the length of the pattern doesn't match " +
-            "the number of space separated substrings", function() {
-        var pattern = [1];
-        var string = "cat dog dog cat";
-        var result = pattern_and_string.checkStringForPattern(pattern, string);
-        expect(result).toBe(false);
-    });
+        "the number of space separated substrings",
+        function() {
+            var pattern = [1];
+            var string = "cat dog dog cat";
+            var result = checkStringForPattern(pattern, string);
+            expect(result).toBe(false);
+        });
 
-    it("should return true if the pattern matches the string", function() {
-        var pattern = [1, 2, 2, 1];
-        var string = "cat dog dog cat";
-        var result = pattern_and_string.checkStringForPattern(pattern, string);
-        expect(result).toBe(true);
-    });
+    it("should return true if the pattern matches the string",
+        function() {
+            var pattern = [1, 2, 2, 1];
+            var string = "cat dog dog cat";
+            var result = checkStringForPattern(pattern, string);
+            expect(result).toBe(true);
+        });
 
-    it("should return false if the pattern doesn't match the string", function() {
-        var pattern = [1, 2, 2, 1];
-        var string = "dog dog cat cat";
-        var result = pattern_and_string.checkStringForPattern(pattern, string);
-        expect(result).toBe(false);
-    });
+    it("should return false if the pattern doesn't match the string",
+        function() {
+            var pattern = [1, 2, 2, 1];
+            var string = "dog dog cat cat";
+            var result = checkStringForPattern(pattern, string);
+            expect(result).toBe(false);
+        });
 
     it("should return false if the pattern has different keys " +
-            "matching the same substring", function() {
-        var pattern = [1, 2, 3, 4];
-        var string = "cat cat cat cat";
-        var result = pattern_and_string.checkStringForPattern(pattern, string);
-        expect(result).toBe(false);
-    });
+        "matching the same substring",
+        function() {
+            var pattern = [1, 2, 3, 4];
+            var string = "cat cat cat cat";
+            var result = checkStringForPattern(pattern, string);
+            expect(result).toBe(false);
+        });
 
     it("should return false if the pattern has the same key " +
-            "matching different substrings", function() {
-        var pattern = [1, 1, 1, 1];
-        var string = "cat dog horse bird";
-        var result = pattern_and_string.checkStringForPattern(pattern, string);
-        expect(result).toBe(false);
-    });
+        "matching different substrings",
+        function() {
+            var pattern = [1, 1, 1, 1];
+            var string = "cat dog horse bird";
+            var result = checkStringForPattern(pattern, string);
+            expect(result).toBe(false);
+        });
 });
--- spec/test2-spec.js
+++ spec/test2-spec.js
@@ -5,36 +5,50 @@ var perfect_number = require('../src/js/perfect-number');
 describe('isPerfectNumber', function() {
     var isPerfectNumber = perfect_number.isPerfectNumber;
 
-    it("should return 'false' if the number is not an integer", function() {
-        var result = isPerfectNumber(1.5);
-        expect(result).toBe(false);
-    });
+    it("should return 'false' if the number is not an integer",
+        function() {
+            var result = isPerfectNumber(1.5);
+            expect(result).toBe(false);
+        });
 
-    it("should return 'false' if the number is negative", function() {
-        var result = isPerfectNumber(-6);
-        expect(result).toBe(false);
-    });
+    it("should return 'false' if the number is negative",
+        function() {
+            var result = isPerfectNumber(-6);
+            expect(result).toBe(false);
+        });
 
-    it("should return 'false' if the number is odd", function() {
-        var result = isPerfectNumber(3);
-        expect(result).toBe(false);
-    });
+    it("should return 'false' if the number is odd",
+        function() {
+            var result = isPerfectNumber(3);
+            expect(result).toBe(false);
+        });
 
-    it("should return 'true' for known perfect numbers in the range of javascript positive integers", function() {
-        var result =
-            [6, 28, 496, 8128, 33550336, 8589869056, 137438691328]
-                .every(function(n) {
-                    return isPerfectNumber(n);
-                });
-        expect(result).toBe(true);
-    });
+    it("should return 'true' for known perfect numbers " +
+        "in the range of javascript positive integers",
+        function() {
+            var knownPerfectNumbers = [6, 28, 496, 8128, 33550336, 8589869056, 137438691328];
+            var result = knownPerfectNumbers.every(function(n) {
+                return isPerfectNumber(n);
+            });
+            expect(result).toBe(true);
+        });
 
-    it("should return 'false' for non perfect numbers", function() {
-        var result =
-            [16, 128, 1496, 9999, 34467896, 8999999956, 111111111118]
-                .some(function(n) {
-                    return isPerfectNumber(n);
-                });
-        expect(result).toBe(false);
-    });
+    it("should return 'false' for non perfect numbers",
+        function() {
+            var knownPerfectNumbers = [6, 28, 496, 8128, 33550336, 8589869056, 137438691328];
+            var nonPerfectIntegers = [];
+            // fill the array with non perfect numbers
+            for (var i = 0; i < 100; i++) {
+                var rnd = Math.floor(Math.random() * 10000);
+                    // if it's a known perfect number, change it
+                while (rnd in knownPerfectNumbers) {
+                    rnd = Math.floor(Math.random() * 10000);
+                }
+                nonPerfectIntegers.push(rnd);
+            }
+            var result = nonPerfectIntegers.some(function(n) {
+                return isPerfectNumber(n);
+            });
+            expect(result).toBe(false);
+        });
 });
